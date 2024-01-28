0cbc81f94b6655d455bdce15728f4ef5ffaa87e5
Sakhra <Sakhranoor@gmail.com>
2024-01-28 15:46:07 +0500

    add door class defination

---
+++ Door.cpp
@@ -0,0 +1,22 @@
+
+
+#include<iostream> 
+using namespace std;
+
+class Door {
+private:
+    bool isopen;
+    bool isLocked;
+
+public:
+Door()// Initializes the door as closed and unlocked
+void openDoor();
+ void closeDoor();
+void lockDoor();
+void unlock();
+bool isDoorOpen()const;
+bool isDoorLocked() const;
+
+int main(){
+return 0;
+}
